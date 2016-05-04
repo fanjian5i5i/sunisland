e40e427aff9bd9e8e9f5f1ea8f139d40ff6c297b
Jian Fan <jian.fan@boston.gov>
2016-05-04 16:41:28 -0400

    json stores fielddata

--- client/components/inputtab/generalinfo.jsx
+++ client/components/inputtab/generalinfo.jsx
@@ -16,9 +16,26 @@ Generalinfo = React.createClass({
             email:this.data && this.data.currentUser && this.data.currentUser.emails ? this.data.currentUser.emails[0].address:'you@yourdomain.com'
         }
     },
-    goNext(){
-        console.log(this.state);
-        FlowRouter.go('/newmodule/electricalinfo');
+    saveAndContinue(e){
+      e.preventDefault()
+      var data = {
+       moduleName : this.refs.moduleName.value,
+       poo : this.refs.poo.value,
+       technology : this.refs.technology.value,
+       moduleName : this.refs.moduleName.value,
+       type : this.refs.type.value,
+       dualglass : this.refs.dualglass.value,
+       antipid : this.refs.antipid.value,
+       snowload : this.refs.snowload.value,
+       windload : this.refs.windload.value,
+       moduleName : this.refs.moduleName.value,
+       keyfeature : this.refs.keyfeature.value,
+
+       }
+      this.props.saveValues(data)
+      this.props.nextStep()
+      console.log(this.state);
+        // FlowRouter.go('/newmodule/electricalinfo');
     },
     render(){
         // console.log(this.state);
@@ -29,12 +46,12 @@ Generalinfo = React.createClass({
                       </div>
                       <div className="panel-body">
                         <div className="form-group">
-                          <label htmlFor="usr">Module Name:</label>
-                          <input type="text" className="form-control" id="usr"/>
+                          <label htmlFor="moduleName">Module Name:</label>
+                          <input type="text" className="form-control" id="usr" ref="moduleName"/>
                         </div>
                         <div className="form-group">
                           <label htmlFor="poo">Place of Origin:</label>
-                          <select className="form-control" id="poo">
+                          <select className="form-control" id="poo" ref="poo">
                             <option>China</option>
                             <option>Europe</option>
                             <option>USA</option>
@@ -42,28 +59,28 @@ Generalinfo = React.createClass({
                         </div>
                         <div className="form-group">
                           <label htmlFor="technology">Technology:</label>
-                          <select className="form-control" id="technology">
+                          <select className="form-control" id="technology" ref="technology">
                             <option>Monocrystalline</option>
                             <option>Multicrystalline</option>
                           </select>
                         </div>
                         <div className="form-group">
                           <label htmlFor="type">Type:</label>
-                          <select className="form-control" id="type">
+                          <select className="form-control" id="type" ref="type">
                             <option>N</option>
                             <option>P</option>
                           </select>
                         </div>
                         <div className="form-group">
                           <label htmlFor="dualglass">Dual glass:</label>
-                          <select className="form-control" id="dualglass">
+                          <select className="form-control" id="dualglass" ref="dualglass">
                             <option>Yes</option>
                             <option>No</option>
                           </select>
                         </div>
                         <div className="form-group">
                           <label htmlFor="antipid">Anti-PID:</label>
-                          <select className="form-control" id="antipid">
+                          <select className="form-control" id="antipid" ref="antipid">
                             <option>Yes</option>
                             <option>No</option>
                           </select>
@@ -71,23 +88,23 @@ Generalinfo = React.createClass({
                         <div className="form-group">
                             <label htmlFor="snowload">Snow Load:</label>
                             <div className="input-group">
-                                <input id="snowload" type="number" className="form-control" aria-describedby="basic-addon1"/>
+                                <input id="snowload" type="number" className="form-control" aria-describedby="basic-addon1" ref="snowload"/>
                                 <span className="input-group-addon" id="basic-addon1">Pa</span>
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="windload">Wind Load:</label>
                             <div className="input-group">
-                                <input id="windload" type="number" className="form-control" aria-describedby="basic-addon2"/>
+                                <input id="windload" type="number" className="form-control" aria-describedby="basic-addon2" ref="windload"/>
                                 <span className="input-group-addon" id="basic-addon2">Pa</span>
                             </div>
                         </div>
                         <div className="form-group">
                           <label htmlFor="keyfeature">Key Features:</label>
-                          <textarea className="form-control" rows="5" id="keyfeature"></textarea>
+                          <textarea className="form-control" rows="5" id="keyfeature" ref="keyfeature"></textarea>
                         </div>  
                         <div className="form-group">
-                          <button type="button" className="btn btn-success pull-right" onClick={this.goNext}>Next</button>
+                          <button type="button" className="btn btn-success pull-right" onClick={this.saveAndContinue}>Save And Continue</button>
                         </div> 
                       </div>
                     </div>
--- client/components/inputtab/inputtab.jsx
+++ client/components/inputtab/inputtab.jsx
@@ -1,5 +1,15 @@
+var fieldValues = {
+         name : null,
+         email : null,
+         password : null,
+         age : null,
+         colors : []
+}
+
 Inputtab = React.createClass({
+    
     mixins: [ReactMeteorData],
+    
     getMeteorData(){
         let data = {};
         data.currentUser = {};
@@ -9,25 +19,73 @@ Inputtab = React.createClass({
         }
         return data;
     },
+    
     getInitialState(){
         return {
+            step: 1,
             klass:'img-circle img-responsive custom-input-file',
             editmode:false,
             email:this.data && this.data.currentUser && this.data.currentUser.emails ? this.data.currentUser.emails[0].address:'you@yourdomain.com'
         }
     },
-    goNext(){
-        console.log(this.state);
+    saveValues(fields) {
+     console.log(fieldValues);
+     return function() {
+     // Remember, `fieldValues` is set at the top of this file, we are simply appending
+     // to and overriding keys in `fieldValues` with the `fields` with Object.assign
+     // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+     fieldValues = Object.assign({}, fieldValues, fields)
+     }()
+    },
+
+    nextStep() {
+     this.setState({
+     step : this.state.step + 1
+     })
+    },
+
+    // Same as nextStep, but decrementing
+    previousStep() {
+     this.setState({
+     step : this.state.step - 1
+     })
     },
     render(){
-        // console.log(this.state);
-        return (
-                <div className="row">
-                    <div className="container">
-                        <Generalinfo/>
-                        
-                    </div>
-                </div>
-        )
+
+        var fieldValues = {
+             name : null,
+             email : null,
+             password : null,
+             age : null,
+             colors : []
+        };
+
+             switch(this.state.step) {
+             case 1:return (
+                        <div className="row">
+                            <div className="container">
+                                <Generalinfo fieldValues={fieldValues} nextStep={this.nextStep} saveValues={this.saveValues}/>
+                                
+                            </div>
+                        </div>
+                    )
+             case 2:return (
+                        <div className="row">
+                            <div className="container">
+                                <Electricalinfo/>
+                                
+                            </div>
+                        </div>
+                    )
+             case 3:return (
+                        <div className="row">
+                            <div className="container">
+                                <Mechanicalinfo/>
+                                
+                            </div>
+                        </div>
+                    )
+             }
+            
     }
 });
